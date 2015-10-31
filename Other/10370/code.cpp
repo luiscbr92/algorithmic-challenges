2b4a1a7766a22a9ab366f1da0680fdf887aef663
luiscbr92 <luiscbr92@gmail.com>
2015-10-31 03:15:58 +0100

    UVa 10370 solved

---
+++ Other/10370/code.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio (false);
+
+    int cases, c, students, i, above;
+    int grades[1000];
+    double average;
+
+    cin >> cases;
+    for(c = 0; c < cases; c++){
+        cin >> students;
+        average = 0;
+        above = 0;
+
+        for(i = 0; i < students; i++){
+            cin >> grades[i];
+            average += grades[i];
+        }
+        average /= (double)students;
+
+        for(i = 0; i < students; i++){
+            if(grades[i] > average) above++;
+        }
+
+        printf("%.3f%%\n", 100.0*above/students);
+    }
+
+    return 0;
+}
