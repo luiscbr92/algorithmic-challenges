a918477306c9cf78829d25c58953f003d3097893
luiscbr92 <luiscbr92@gmail.com>
2015-10-31 01:19:08 +0100

    UVa 10071 solved

---
+++ Other/10071/code.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio (false);
+
+    int a, b;
+    while(cin >> a >> b) cout << ((a*b)<<1) << endl;
+
+    return 0;
+}
