0d35a6d402a1f14686980177f4634eeef3bda7b9
luiscbr92 <luiscbr92@gmail.com>
2016-04-04 18:03:37 +0200

    UVa 13079 changed

--- UVaOnlineJudge/Contest_Volumes(10000...)/Volume130(13000-13099)/13079/code.cpp
+++ UVaOnlineJudge/Contest_Volumes(10000...)/Volume130(13000-13099)/13079/code.cpp
@@ -1,49 +1,38 @@
 #include <iostream>
+#include <algorithm>
+#include <utility>
 #include <vector>
 
 using namespace std;
+
+bool my_comp(pair<int,int> p1, pair<int,int> p2){
+    return p1.second < p2.second;
+}
 
 int main(){
     ios_base::sync_with_stdio (false);
 
     int number_of_buildings, tmp_east, tmp_west, i, j, tunnels;
-    vector<int> west;
-    vector<int> east;
+    vector<pair<int,int>> buildings;
+    pair<int,int> tmp_building;
 
     while(cin >> number_of_buildings && number_of_buildings){
-        vector<bool> connected (number_of_buildings, false);
-        west.clear();
-        east.clear();
+        buildings.clear();
         tunnels = 0;
-
+        
         for(i = 0; i < number_of_buildings; i++){
             cin >> tmp_west >> tmp_east;
-            west.push_back(tmp_west);
-            east.push_back(tmp_east);
+            tmp_building = make_pair(tmp_west, tmp_east);
+            buildings.push_back(tmp_building);
         }
+        sort(buildings.begin(), buildings.end(), my_comp);
 
-        for(i = 1; i < number_of_buildings; i++){
-            j = i-1;
-            tmp_east = east[i];
-            tmp_west = west[i];
-            while(j >= 0 && east[j] > tmp_east){
-                east[j+1] = east[j];
-                west[j+1] = west[j];
-                j--;
-            }
-            east[j+1] = tmp_east;
-            west[j+1] = tmp_west;
-        }
-
-        for(i = 0; i < number_of_buildings; i++){
-            if(!connected[i]){
-                tunnels++;
-                j = i;
-                while(j < number_of_buildings && west[j] < east[i]){
-                    connected[j] = true;
-                    j++;
-                }
-            }
+        i = 0;
+        while(i < number_of_buildings){
+            tunnels++;
+            j = i;
+            while(j < number_of_buildings && buildings[j].first < buildings[i].second) j++;
+            i = j;
         }
 
         cout << tunnels << '\n';
