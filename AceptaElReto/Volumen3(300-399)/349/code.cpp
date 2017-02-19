6e31fee8767caaf2dfef07a9b2a9fac5df872433
luiscbr92 <luiscbr92@gmail.com>
2017-02-19 22:05:03 +0100

    AceptaElReto 349 solved

---
+++ AceptaElReto/Volumen3(300-399)/349/code.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <stack>
+
+#define endl        '\n'
+
+using namespace std;
+
+int main(){
+
+  int cases, a, b;
+  string line;
+  stack<int> op;
+
+  cin >> cases;
+  cin.ignore();
+  while(cases--){
+    getline(cin, line);
+    //cout << line << endl;
+    while(!op.empty())
+      op.pop();
+
+    for(int i = line.size()-1; i >= 0; i -= 2){
+      //cout << line[i] << endl;
+      if(line[i] == '+'){
+        a = op.top();
+        op.pop();
+        b = op.top();
+        op.pop();
+        op.push(a + b);
+      }
+      else if(line[i] == '-'){
+        a = op.top();
+        op.pop();
+        b = op.top();
+        op.pop();
+        op.push(a - b);
+      }
+      else if(line[i] == '*'){
+        a = op.top();
+        op.pop();
+        b = op.top();
+        op.pop();
+        op.push(a * b);
+      }
+      else if(line[i] == '/'){
+        a = op.top();
+        op.pop();
+        b = op.top();
+        op.pop();
+        op.push(a / b);
+      }
+      else{
+        op.push(line[i] - '0');
+      }
+    }
+    cout << op.top() << endl;
+  }
+
+  return 0;
+}
